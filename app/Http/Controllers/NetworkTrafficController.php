cb21d757775b44b4eda4532a0c8a31b2c5183104
viniciusxv27 <vinicius8cm@gmail.com>
2024-12-03 12:13:49 -0300

    feat: edit endpoint for send packets

--- app/Http/Controllers/NetworkTrafficController.php
+++ app/Http/Controllers/NetworkTrafficController.php
@@ -13,7 +13,27 @@ class NetworkTrafficController extends Controller
 
     public function send(Request $request)
     {
-        //Receber o envio de pacotes
+        $data = $request->all();
+        $networkTraffic = new NetworkTraffic();
+        $networkTraffic->mac_origin = $data['mac_origem'];
+        $networkTraffic->mac_destination = $data['mac_destino'];
+        $networkTraffic->protocol_version = $data['ip_versao'];
+        $networkTraffic->ip_origin = $data['ip_origem'];
+        $networkTraffic->ip_destination = $data['ip_destino'];
+        $networkTraffic->protocol = $data['protocolo'];
+        $networkTraffic->application_protocol = $data['protocolo'];
+        $networkTraffic->origin_port = $data['porta_origem'];
+        $networkTraffic->destination_port = $data['porta_destino'];
+        $networkTraffic->payload = $data['payload'];
+        $networkTraffic->network_id = $data['network_id'];
+        $networkTraffic->save();
+
+        $network = Network::find($data['network_id']);
+        $network->status = 'up';
+        $network->mb_used = NetworkTraffic::where('network_id', $data['network_id'])->sum('payload') / (1024 * 1024);
+        $network->save();
+
+        return ['status' => 'success'];
     }
 
 }
